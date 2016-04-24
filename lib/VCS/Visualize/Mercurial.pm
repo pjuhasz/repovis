b5bd4586c98505fbb702bd11ac84aa11c9ac5c67
Juhász Péter <peter.juhasz83@gmail.com>
2016-04-24 21:16:01 +0200

    -Add: perl module boilerplate

---
+++ lib/VCS/Visualize/Mercurial.pm
@@ -0,0 +1,11 @@
+package VCS::Visualize::Mercurial;
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub new {
+
+}
+
+1;
---
+++ lib/VCS/Visualize/Mercurial/CmdServer.pm
@@ -0,0 +1,15 @@
+package VCS::Visualize::Mercurial::CmdServer;
+
+use 5.010001;
+use strict;
+use warnings;
+
+
+sub new {
+
+}
+
+
+
+
+1;
