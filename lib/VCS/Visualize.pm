128275c8691e6a0098041129b5c2b9b4c079320f
Juhász Péter <peter.juhasz83@gmail.com>
2016-04-26 01:10:21 +0200

    -Change: trace region borders, write binary avs and separate text files, refactor

--- lib/VCS/Visualize.pm
+++ lib/VCS/Visualize.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 use Module::Load;
-use List::Util qw/sum shuffle any/;
+use List::Util qw/sum shuffle first/;
 use File::Basename;
 use POSIX qw/floor/;
 use Carp;
@@ -35,7 +35,7 @@ sub new {
 
 	my $curve_module = $args{curve_module} // 'AR2W2Curve';
 	carp "error: '$curve_module' is not a valid Math::PlanePath module, it must be one of the following:\n" .
-	(join ", ", @curve_modules) . "\n" if not any {$_ eq $curve_module} @curve_modules;
+	(join ", ", @curve_modules) . "\n" if not first {$_ eq $curve_module} @curve_modules;
 	my $curve_class = 'Math::PlanePath::'.$curve_module;
 	load $curve_class;
 
@@ -49,6 +49,9 @@ sub new {
 		files => {},
 		filetypes => {},
 		users => {},
+		xs => -1,
+		ys => -1,
+		output_dir => '.repovis',
 	};
 
 	srand(1234);
@@ -61,54 +64,190 @@ sub analyze_one_rev {
 
 	$rev //= $self->{repo}->current_rev();
 
-	my $max_numeric_id = $self->{repo}->numeric_id($rev);
+	$self->{max_numeric_id} = $self->{repo}->numeric_id($rev);
 
 	my $files = $self->{repo}->files();
 
-	my @file_color_idxs = shuffle (0..(-1+scalar @$files));
+	$self->{lcnt} = 0;
+	$self->{fcnt} = 0;
 
-	my $lcnt = 0;
-	my $fcnt = 0;
+	my ($max_x, $max_y, $min_x, $min_y) = (-1000000, -1000000, 1000000, 1000000);
 
 	for my $file (@$files) {
-		my $blame = $self->{repo}->blame($file);
-		next if $blame->[0] =~ /binary file/ or @$blame == 0;
-
-		my ($ext) = ($file =~ /\.(\w+)$/);
-		$ext //= $file;
-		$self->{filetypes}{$ext}{hue} //= 280 * rand();
-		$self->{files}{$file}{hue} = 72 * rand() + $self->{filetypes}{$ext}{hue};
-
-		my (@x, @y);
-		for my $line (@$blame) {
-			if (my ($user, $id, $crev) = ($line =~ / \s* (.*?) \s+ (\d+) \s+ ([\da-f]+): /x)) {
-				$self->{users}{$user} //= { n => scalar keys %{$self->{users}}, hue => 360*rand() };
-
-				my ($x, $y) = $self->{curve}->n_to_xy($lcnt);
-				push @x, $x;
-				push @y, $y;
-
-				my $blame_rgb = hsv2rgb($self->{users}{$user}{hue}, $id/$max_numeric_id, 1);
-				my $file_rgb  = $id == $max_numeric_id ?
-									hsv2rgb(360, 1, 1) : # red
-									hsv2rgb($self->{files}{$file}{hue}, 0.5, 0.8);
-				say join "\t", $user, $self->{users}{$user}{n}, $id, $fcnt, $file_rgb, $blame_rgb, $x, $y;
-				$lcnt++;
-			}
+		$self->do_one_file($file);
+		my $ex = $self->{files}{$file}{extent};
+		if (defined $ex) {
+			$max_x = $max_x > $ex->{max_x} ? $max_x : $ex->{max_x};
+			$max_y = $max_y > $ex->{max_y} ? $max_y : $ex->{max_y};
+			$min_x = $min_x < $ex->{min_x} ? $min_x : $ex->{min_x};
+			$min_y = $min_y < $ex->{min_y} ? $min_y : $ex->{min_y};
+			$self->{fcnt}++;
 		}
-		my $xmean = (sum @x) / (scalar @x);
-		my $ymean = (sum @y) / (scalar @y);
-		push @{$self->{files}{$file}{coords}}, $xmean, $ymean;
+	}
+	
+	$self->{xs} = $max_x-$min_x;
+	$self->{ys} = $max_y-$min_y;
 
-		$fcnt++;
+	$self->grids_from_coords($min_x, $min_y);
+
+	$self->trace_borders();
+	
+	$self->to_disk($rev);
+}
+
+# private methods
+
+sub do_one_file {
+	my ($self, $file) = @_;
+
+	my $blame = $self->{repo}->blame($file);
+	return if $blame->[0] =~ /binary file/ or @$blame == 0;
+
+	my ($ext) = ($file =~ /\.(\w+)$/);
+	$ext //= $file;
+	$self->{filetypes}{$ext}{H} //= 320 * rand();
+	$self->{files}{$file}{H} //= 36 * rand() + $self->{filetypes}{$ext}{H};
+	$self->{files}{$file}{S} //= 0.4+0.2*rand();
+	$self->{files}{$file}{V} //= 0.7+0.2*rand();
+
+	my ($max_x, $max_y, $min_x, $min_y) = (-1000000, -1000000, 1000000, 1000000);
+
+	my @coord_list;
+	for my $line (@$blame) {
+		if (my ($user, $id, $crev) = ($line =~ / \s* (.*?) \s+ (\d+) \s+ ([\da-f]+): /x)) {
+			$self->{users}{$user} //= {
+				n => scalar(keys %{$self->{users}}),
+				H => 360*rand(),
+			};
+
+			my ($x, $y) = $self->{curve}->n_to_xy($self->{lcnt});
+			$max_x = $max_x > $x ? $max_x : $x;
+			$max_y = $max_y > $y ? $max_y : $y;
+			$min_x = $min_x < $x ? $min_x : $x;
+			$min_y = $min_y < $y ? $min_y : $y;
+
+			my $blame_rgb = hsv2rgb($self->{users}{$user}{H}, $id/$self->{max_numeric_id}, 1);
+			my $file_rgb  = $id == $self->{max_numeric_id} ?
+								hsv2rgb(360, 1, 1) : # red
+								hsv2rgb( map { $self->{files}{$file}{$_} } qw/H S V/ );
+
+			push @coord_list, [$x, $y, $file_rgb, $blame_rgb];
+
+			$self->{lcnt}++;
+		}
+	}
+	
+	my ($xmean, $ymean) = (0, 0);
+	for my $pt (@coord_list) {
+		$xmean += $pt->[0];
+		$ymean += $pt->[1];
+	}
+	
+	$xmean /= scalar @coord_list;
+	$ymean /= scalar @coord_list;
+	$self->{files}{$file}{coords} = \@coord_list;
+	$self->{files}{$file}{center} = [$xmean, $ymean];
+	$self->{files}{$file}{extent} = {
+		max_x => $max_x,
+		max_y => $max_y,
+		min_x => $min_x,
+		min_y => $min_y,
+	};
+}
+
+sub grids_from_coords {
+	my ($self, $min_x, $min_y) = @_;
+
+	for my $file (keys %{$self->{files}}) {
+		for my $pt (@{$self->{files}{$file}{coords}}) {
+			$self->{file_grid}[ $pt->[0]-$min_x+1][$pt->[1]-$min_y+1] = $pt->[2];
+			$self->{blame_grid}[$pt->[0]-$min_x+1][$pt->[1]-$min_y+1] = $pt->[3];
+		}
+	}
+}
+
+sub trace_borders {
+	my ($self) = @_;
+	
+	my @border;
+	for my $y (0..($self->{ys}+1)) {
+		for my $x (0..($self->{xs}+1)) {
+			my $v = $self->{file_grid}[$x][$y]//0;
+			push @border, [$x-0.5, $y-1.5, 0, 1]  if ($v != ($self->{file_grid}[$x+1][$y]//0));
+			push @border, [$x-1.5, $y-0.5, 1, 0]  if ($v != ($self->{file_grid}[$x][$y+1]//0));
+		}
 	}
 
-	print "\n\n";
+	@border = sort { $a->[0] <=> $b->[0] || $a->[1] <=> $b->[1] } @border;
 
+	for my $i (0..$#border-1) {
+		if ($border[$i][1] + $border[$i][3] == $border[$i+1][1] and $border[$i][0] == $border[$i+1][0] and $border[$i+1][2] == 0) {
+			$border[$i+1][1] = $border[$i][1];
+			$border[$i+1][3] = $border[$i][3] + 1;
+			$border[$i][3] = 0;
+		}
+	}
+
+	@border = grep { $_->[2] || $_->[3] } @border;
+
+	@border = sort { $a->[1] <=> $b->[1] || $a->[0] <=> $b->[0] } @border;
+
+	for my $i (0..$#border-1) {
+		if ($border[$i][0] + $border[$i][2] == $border[$i+1][0] and $border[$i][1] == $border[$i+1][1] and $border[$i+1][3] == 0) {
+			$border[$i+1][0] = $border[$i][0];
+			$border[$i+1][2] = $border[$i][2] + 1;
+			$border[$i][2] = 0;
+		}
+	}
+
+	@border = grep { $_->[2] || $_->[3] } @border;
+
+	$self->{borders} = \@border;
+}
+
+sub to_disk {
+	my ($self, $rev) = @_;
+
+	$self->print_binary_matrix($rev.'_f.dat', 'file_grid');
+	$self->print_binary_matrix($rev.'_b.dat', 'blame_grid');
+	$self->print_files($rev.'_l.dat');
+	$self->print_borders($rev.'_c.dat');
+}
+
+# gnuplot recognizes this format as AVS
+sub print_binary_matrix {
+	my ($self, $fn, $key) = @_;
+	
+	open (my $fh, '>', $fn) or carp "can't open $fn";
+	binmode($fh);
+
+	print {$fh} pack 'L L', ($self->{xs}+1), ($self->{ys}+1);
+
+	for my $y (1..$self->{ys}+1) {
+		for my $x (1..$self->{xs}+1) {
+			print {$fh} pack 'L>', ($self->{$key}[$x][$y] // 0xffffff); 
+		}
+	}
+	close $fh;
+}
+
+sub print_files {
+	my ($self, $fn) = @_;
+	
+	open (my $fh, '>', $fn) or carp "can't open $fn";
 	for my $file (sort keys %{$self->{files}}) {
 		my $basename = basename($file);
-		say join "\t", qq{"$basename"}, @{$self->{files}{$file}{coords}};
+		say {$fh} join "\t", qq{"$basename"}, @{$self->{files}{$file}{center}} if defined $self->{files}{$file};
 	}
+	close $fh;
+}
+
+sub print_borders {
+	my ($self, $fn) = @_;
+
+	open (my $fh, '>', $fn) or carp "can't open $fn";
+	say {$fh} join "\t", @$_ for @{$self->{borders}};
+	close $fh;
 }
 
 sub hsv2rgb {
@@ -147,7 +286,6 @@ sub hsv2rgb {
 	}
 	return (int($r*255.9)<<16) + (int($g*255.9)<<8) + int($b*255.9);
 }
-
 
 1;
 __END__
