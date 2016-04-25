b5e5be127a2e35a353355d66e3f485a1cdb164f2
Peter Juhasz <juhasz.peter@uhusystems.com>
2016-04-25 13:17:17 +0200

    -Fix: require, or rather Module::Load the repo-specific class in Repo.pm

--- lib/VCS/Visualize/Repo.pm
+++ lib/VCS/Visualize/Repo.pm
@@ -4,20 +4,20 @@ use 5.010001;
 use strict;
 use warnings;
 use Carp;
-
+use Module::Load;
 
 sub new {
 	my %args = @_;
 	my @modules = qw/Mercurial/;
 	
-	my $dir = $self->{dir} // '.';
+	my $dir = $args{dir} // '.';
 	
 	for my $module (@modules) {
-		no strict 'ref';
 		my $class = 'VCS::Visualize::Repo::'.$module;
 		my $identify = $class.'::identify';
 		my ($node, $numeric) = $identify->($dir);
 		if (defined $node) {
+			load $class;
 			return $class->new(%args, orig_node => $node, orig_id => $numeric);
 		}
 	}
